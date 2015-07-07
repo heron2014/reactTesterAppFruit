4960a9a28d28d53094dbb3ecbc1390efab85cf4a
Anita <a.nita@hotmail.co.uk>
2015-07-07 16:47:17 +0100

    initial set up

---
+++ src/js/components/AppFruit.jsx
@@ -0,0 +1,14 @@
+var React = require("react");
+
+var AppFruit = React.createClass({
+  render: function() {
+    return (
+      <h1>hello</h1>
+    );
+  }
+
+});
+
+
+module.exports = AppFruit;
+
---
+++ src/js/main.jsx
@@ -0,0 +1,10 @@
+var React = require("react");
+var rootEl = document.getElementById("content");
+var AppFruit = require("./components/AppFruit.jsx");
+
+React.render(
+  <AppFruit />,
+  rootEl
+);
+
+
